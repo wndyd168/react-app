1ac0c29cc1ba623ea02a6ae328fb690e28060e88
DESKTOP-THDBVSR\나 <skdiwndyd@gmail.com>
2024-01-29 11:59:06 +0900

    과제 1 creat 생성

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,37 +1,45 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React from 'react'
 import './ExpenseForm.css';
 
-export default class ExpenseForm extends Component {
-    render() {
-        return (
-            <form>
-                <div classname='form-center'>
-                    <div classname='form-group'>
-                        <label htmlFor='charge'>상품</label>
-                        <input
-                            type='text'
-                            className='form-control'
-                            id='charge'
-                            name='charge'
-                            placeholder='예) 콜라'
-                        />
-                    </div>
-                    <div classname='form-group'>
-                        <label htmlFor='amount'>비용</label>
-                        <input
-                            type='number'
-                            className='form-control'
-                            id='amount'
-                            name='amount'
-                            placeholder='예) 100'
-                        />
-                    </div>
-
+// export default class ExpenseForm extends Component {
+const ExpenseForm = ({ charge, handleCharge, amount, handleAmount, handleSubmit }) => {
+    // render() {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className='form-center'>
+                <div className='form-group'>
+                    <label htmlFor='charge'>상품</label>
+                    <input
+                        type='text'
+                        className='form-control'
+                        id='charge'
+                        name='charge'
+                        placeholder='예) 콜라'
+                        value={charge}
+                        onChange={handleCharge}
+                    />
                 </div>
-                <button type='submit' classname='btn'>
-                    제출
-                </button>
-            </form>
-        )
-    }
+                <div className='form-group'>
+                    <label htmlFor='amount'>비용</label>
+                    <input
+                        type='number'
+                        className='form-control'
+                        id='amount'
+                        name='amount'
+                        value={amount}
+                        placeholder='예) 100'
+                        onChange={handleAmount}
+                    />
+                </div>
+
+            </div>
+            <button type='submit' className='btn'>
+                제출
+            </button>
+        </form>
+    )
+
 }
+
+export default ExpenseForm;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react'
 import './ExpenseList.css'
-export default class ExpenseList extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <ul className='list'>
-
-                </ul>
-                <button className='btn'>
-                    목록 지우기
+import ExpenseItem from './ExpenseItem'
 
 
-                </button>
-            </React.Fragment>
-        )
-    }
+const ExpenseList = ({ initialExpenses, handleDelete }) => {
+    return (
+        <React.Fragment>
+            <ul className='list'>
+                {initialExpenses.map(expense => {
+                    return (
+                        <ExpenseItem key={expense.id} expense={expense}
+                            handleDelete={handleDelete}
+                        />
+
+                    )
+                })}
+
+            </ul>
+            <button className='btn'>
+                목록 지우기
+
+
+            </button>
+        </React.Fragment>
+    )
 }
+
+export default ExpenseList
+
+
+
+
+// export default class ExpenseList extends Component {
+
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <ul className='list'>
+//                     {this.props.initialExpenses.map(expense => {
+//                         return (
+//                             <ExpenseItem key={expense.id} expense={expense}
+//                                 handleDelete={this.props.handleDelete}
+//                             />
+
+//                         )
+//                     })}
+
+//                 </ul>
+//                 <button className='btn'>
+//                     목록 지우기
+
+
+//                 </button>
+//             </React.Fragment>
+//         )
+//     }
+// }
